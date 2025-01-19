c8761a6675aa7651de660c2e325224c3d2e6cfd4
EvgeniyNesterov <105553884+NesterovEvgeniy@users.noreply.github.com>
2025-01-19 17:40:59 +0300

    Add files of test bot

---
+++ bot.py
@@ -0,0 +1,22 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from config import TOKEN
+from handlers import router
+from middlewares import LoggingMiddleware
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+dp.include_router(router)
+dp.message.middleware(LoggingMiddleware())
+
+
+
+
+
+async def main():
+  print("Бот запущен!")
+  await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+  asyncio.run(main())
